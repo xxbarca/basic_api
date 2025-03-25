7f7a4b6be7d1ac6d9663bd9c902cc36b40d8c20d
liyang <xxbarca@163.com>
2025-03-25 12:43:11 +0800

    fake userinfo

--- src/modules/Auth/controllers/user.controller.ts
+++ src/modules/Auth/controllers/user.controller.ts
@@ -1,14 +1,15 @@
 import { Controller, Get } from '@nestjs/common';
+import { UnifyResponse } from '@/common/Interceptors';
 
 @Controller('user')
 export class UserController {
   @Get('info')
   userInfo() {
-    return {
+    return UnifyResponse.success({
       id: 0,
       realName: 'Vben',
       roles: ['super'],
       username: 'vben',
-    };
+    });
   }
 }

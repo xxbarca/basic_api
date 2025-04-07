22d6054f4a75a7fa55d1fcf681b20e854767f336
liyang <xxbarca@163.com>
2025-04-07 22:06:51 +0800

    登录

--- src/main.ts
+++ src/main.ts
@@ -11,6 +11,7 @@ async function bootstrap() {
   useContainer(app.select(AppModule), {
     fallbackOnErrors: true,
   });
+  app.enableCors();
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
